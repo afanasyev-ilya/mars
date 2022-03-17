47489157cfdaac68d6a455a1632838391ec29758
Elijah <afanasiev_ilya@icloud.com>
2022-03-17 21:47:52 +0300

    - unused setting removed

--- settings.hpp
+++ settings.hpp
@@ -1,9 +1,4 @@
 #pragma once
 
 #define base_type double
-
-//#define __USE_CUDA__
-#define __USE_CUBLAS__
 #define MAX_PRINTING_SIZE 1000
-
-#define MEM_SIZE 4 // assume we have 4 GB of memory to store matrix and spins for different MC steps
